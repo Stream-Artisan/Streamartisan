098f2ff41287b64096d7a2cdce7fa492069149b5
AliAbdullah1999 <alithedeveloper1999@gmail.com>
2025-07-09 23:06:11 +0500

    navbar top bar made better

---
+++ scripts/scrol.js
@@ -0,0 +1,29 @@
+  let lastScrollTop = 0;
+      const navbar = document.querySelector(".navbar");
+
+      // Hide navbar on page load if not at the top
+      window.addEventListener("load", () => {
+        const scrollTop =
+          window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > 0) {
+          navbar.style.top = "-200px";
+        } else {
+          navbar.style.top = "0";
+        }
+      });
+
+      window.addEventListener("scroll", () => {
+        const scrollTop =
+          window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+          // Scroll down
+          navbar.style.top = "-200px";
+        } else {
+          // Scroll up
+          navbar.style.top = "0";
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+      });
